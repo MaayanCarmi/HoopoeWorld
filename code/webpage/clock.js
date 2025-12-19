aa2a2e1e71d93199202869115cc1064d37013931
Maayan Carmi <maayan.carmi.mc@gmail.com>
2025-12-19 20:17:49 +0200

    create fake webpage
    create the webpage with fake data, and without connecting to a server.
    it was mostly to see how it will look like but also understand what to do and create the needed css.
    a lot of the stuff in the html will be changes in server (like almost everything) and I also need to create the refresh and GET functions. so still have work but those are the fake stuff. and I don't really think I need comments here.

---
+++ code/webpage/clock.js
@@ -0,0 +1,12 @@
+﻿setInterval(myTime, 1000);
+
+function myTime() {
+    var d = new Date();
+
+    // Use toUTCString() for the date and time
+    var date = d.toUTCString().split(' ')[0] + " " + d.toUTCString().split(' ')[1] + " " + d.toUTCString().split(' ')[2] + " " + d.toUTCString().split(' ')[3];
+    var time = d.toUTCString().split(' ')[4];  // Just the time part
+
+    var finalTime = date + "</br>" + time + " UTC";
+    document.getElementById("clock").innerHTML = finalTime;
+}
